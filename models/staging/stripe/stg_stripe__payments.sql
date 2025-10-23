e444888ffbc6f28e06b3eceaa8098c41f35f5915
Atakan Birbir <birbirab@gmail.com>
2025-10-23 20:57:44 +0000

    fuck u

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,9 +3,7 @@
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-
-
         amount/100 as amount,
         created as created_at
 
-    from raw.stripe.payments
+    from {{ source('stripe', 'payments') }}
